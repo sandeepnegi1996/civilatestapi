3693dcd584b3481a76d92e247e805cae178f0897
sandeepnegi1996 <9041128179sandy@gmail.com>
2019-09-18 13:38:40 +0530

    facing problem with returning data

--- routes/database/latestData.js
+++ routes/database/latestData.js
@@ -19,12 +19,85 @@ router.post("/latestrecord",(req,res)=>{
 	    	};
 	    
 	    console.log(`post request table name is ${data.dbTableName}`);
-	    //res.send(data.dbTableName);
+	    
+	     dbQueryTableName = data.dbTableName;
+
+	    var params = {
+	        TableName: dbQueryTableName,
+	        KeyConditionExpression: "#deviceid = :deviceidvalue",
+	        ExpressionAttributeNames: {
+	            "#deviceid": "DeviceId"
+	        },
+	        ExpressionAttributeValues: {
+	            ":deviceidvalue": "nodecivi"
+	        },
+	        Limit: 1,
+	        ScanIndexForward: false
+	    };
+	    //params for the request is being set
+
+//=================================================================================
+
+		   var finalDataObj;
+		   var finaldataReturn;
+
+		 ddb.query(params, function (err, data) {
+	        console.log("inside query");
+	        if (err) {
+	            console.log("Error", err);
+	        } else {
+	           //    console.log(data);
+	            // console.log(data.Items[0].DeviceId);
+	             console.log(data.Items[0].data);
+	             finalDataObj = data.Items[0].data;
+	             
+	            var finaldataReturn = {
+	                DeviceID: finalDataObj.nodeid,
+	                AssetID:finalDataObj.assetId,
+	                ReadingId:finalDataObj.readingId,
+	                AccelerationX: finalDataObj.ax,
+	                AccelerationY: finalDataObj.ay,
+	                AccelerationZ: finalDataObj.az,
+	                Temperature: finalDataObj.temp,
+	                RecordTime:finalDataObj.recordtime
+
+	            }
+
+	            //console.log(finaldataReturn);
+
+	         
+
+
+	        //    console.log("outside function sending back the object");
+	         // return  res.send(finaldataReturn);
+
+
+
+
+
+
+	        }
+    });
+
+
+
+
+	
+		   console.log(finaldataReturn);
+		    
+	    
 	    return res.status(200).send({
 	    	success:'true',
 	    	description:'success received request',
-	    	dataReceived:data.dbTableName
+	    	dataReceived:data.dbTableName,
+	    	message:'just testing',
+	    	finalDataObj1:finaldataReturn
+
+
 	    });
+	    
+
+
 	}
 
 
